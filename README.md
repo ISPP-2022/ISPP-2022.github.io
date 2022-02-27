d00088f722cedb787c5534fc48be28687a819a14
alesancor1 <alesancor1@alum.us.es>
2022-02-27 14:19:07 +0100

    Gatsby Landing Page

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ISPP-2022.github.io
+### Landing page
+
+Landing page for Stacking up
+
+### Running in dev
+
+Install gatsby-cli
+```
+npm install --global gatsby-cli
+```
+
+Clone project
+```
+git clone https://github.com/ISPP-2022/ISPP-2022.github.io.git
+```
+
+Install deps and run
+```
+npm i && npm run develop
+```
+
+### Deploying to ghpages
+
+```
+npm run deploy
+```
---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteTitle: 'Stacking Up', // <title>
+  manifestName: 'StackingUp',
+  manifestShortName: 'Landing Page', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+
+  // social
+
+  authorName: 'StackingUp',
+  authorHeading: 'Busca, Alquila, Guarda',
+  authorSocialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/ISPP-2022',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:stackingup13@gmail.com',
+    },
+  ],
+};
